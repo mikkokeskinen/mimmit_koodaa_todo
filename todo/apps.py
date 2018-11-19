6665c912cf552e5307f6cd47bc003668b45b266f
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-11-19 19:39:37 +0200

    Add automatic model registration to admin

--- todo/apps.py
+++ todo/apps.py
@@ -1,5 +1,27 @@
-from django.apps import AppConfig
+from django.apps import apps, AppConfig
+from django.contrib import admin
+
+# These are for automatic admin registration
+# from: https://avilpage.com/2017/11/django-tricks-auto-register-models-admin.html
+
+
+class ListAdminMixin(object):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields if field.name != "id"]
+        super(ListAdminMixin, self).__init__(model, admin_site)
 
 
 class TodoConfig(AppConfig):
     name = 'todo'
+
+    def ready(self):
+        models = apps.get_models()
+        for model in models:
+            if model._meta.app_label != 'todo':
+                continue
+
+            admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+            try:
+                admin.site.register(model, admin_class)
+            except admin.sites.AlreadyRegistered:
+                pass
